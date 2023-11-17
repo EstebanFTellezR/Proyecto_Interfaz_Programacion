8d4e0683bef086204788f0e16e7149669f4c5041
EstebanFTellezR <141454442+EstebanFTellezR@users.noreply.github.com>
2023-11-17 09:42:17 -0500

    Update Estacionamiento.java
    Cambio del nombre de las variables.

--- src/Estacionamiento.java
+++ src/Estacionamiento.java
@@ -1,13 +1,15 @@
 import java.util.HashMap;
 public class Estacionamiento {
+    
     private int vehiculo;
+    
     public Estacionamiento(int vehiculo) {
         this.vehiculo=vehiculo;
     }
 
-    HashMap <Integer, Integer> esta = new HashMap<>();
+    HashMap <Integer, Integer> lista = new HashMap<>();
 
-    public void nose () {
+    public void marcar () {
         for (int i = 0 ;i <= 29;i++){
             esta.put(i,0);
         }
